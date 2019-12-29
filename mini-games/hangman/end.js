8b19ee7eb08c5a6643f8c3ad82156e336d87cd2e
Ereyx <ereyx.ytb@gmail.com>
2019-12-29 21:13:37 +0100

    Start.js et End.js

---
+++ mini-games/hangman/end.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'fin pendu',
+	description: 'Fin du mini-jeu',
+	execute(message, args) {
+		message.channel.send('Fin du mini-jeu');
+	},
+};
---
+++ mini-games/hangman/start.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'debut pendu',
+	description: 'Commencement du mini-jeu',
+	execute(message, args) {
+		message.channel.send('Commencement du mini-jeu');
+	},
+};
