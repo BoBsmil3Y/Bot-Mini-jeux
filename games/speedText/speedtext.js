d92fff3f4cc23284cac73b6714648232cd2697a9
BoBsmil3Y <antoine-dupont@gmx.fr>
2019-12-31 15:29:23 +0100

    Adding Bacon Ipsum API to get random text

--- games/speedText/speedtext.js
+++ games/speedText/speedtext.js
@@ -1,8 +1,30 @@
 module.exports = {
 	name: 'speedtext',
 	description: 'Lance le jeu speed text',
-	execute(message, args) {
-		message.channel.send(`Commande réussi. message : ${message} arguments : ${args}`);
+	execute(m, args) {
+		console.log(`Commande speedtext lancée.`);
+
+		const axios = require('axios');
+
+		let player = m.author;
+		let channel = m.channel;
+		let text = '';
+
+		axios.get('https://baconipsum.com/api/?type=meat-and-filler&paras=2&format=text')
+		.then(response => {
+			channel.send(response.data).await
+		})
+		.catch(error => {
+			console.log(error);
+		});
+
+		/*channel.awaitMessages((mes => mes.author.id === player.id), { max: 1, time: 60000})
+		.then(collected => {
+			
+		});*/
+
+		// Await message -> filtre id joueur
+		// Faire les calculs de points : https://10fastfingers.com/faq
 
 	},
 };
