94cac8f3ec1844373acfa52f5e572383e704df7b
BoBsmil3Y <antoine-dupont@gmx.fr>
2020-01-09 23:53:34 +0100

    Game working & adding a timer to know our time

--- games/speedText/speedtext.js
+++ games/speedText/speedtext.js
@@ -9,12 +9,14 @@ module.exports = {
 		let text;
 		let player = m.author;
 		let channel = m.channel;
+		let start, end;
 
 		axios.get('https://baconipsum.com/api/?type=meat-and-filler&paras=1&format=text')
 		.then(response => {
 			var temp = response.data.split(/ +/);
-			text = temp[0] + " " + temp[1] + " " +  temp[2] + " " +  temp[3] + " " + temp[4];
+			text = temp[0] + " " + temp[1] + " " +  temp[2] + " " +  temp[3];
 			channel.send(text);
+			start = Date.now();
 		})
 		.catch(error => {
 			console.log(error);
@@ -27,28 +29,31 @@ module.exports = {
 		// Check the wroten text.
 		channel.awaitMessages((mes => mes.author.id === player.id), { max: 1, time: 15000})
 		.then(collected => {;
+			end = Date.now();
 
 			let originalText = text.toLowerCase().split(/ +/);
 			let wrotenText = collected.first().content.toLowerCase().replace('  ', ' ').split(/ +/);
-			let wrong = false;
 
 			if (wrotenText === "annuler") return channel.send("Jeu annulé !").delete(5000);
 
-			for (let i = 0; i < originalText.size; i++) {
+			for (let i = 0; i < originalText.length; i++) {
 
-				if (!wrotenText[i] === originalText[i]) return wrong = true;
+				if (wrotenText[i] !== originalText[i]) return channel.send(`Tu as fait une faute ! Tu as mal écrit le mot ${originalText[i]} ! \nTa version du mot : ${wrotenText[i]}`);
 
 			};
-
-			if(wrong) return channel.send(`Tu as fait une faute ! Tu as mal écrit le mot ${originalText[i]} ! \nTa version du mot : ${wrotenText[i]}`);
-
-			channel.send(`Bravo ${player.username} ! Ton temps est de : XX.XX s`)
+			
+			var time = end - start;
+			channel.send(`Bravo ${player.username} ! Ton temps est de : ${time/1000} s`)
 			
 
+		}).catch(error => {
+			console.log(error);
+			channel.send("Une erreur s'est produite en récupérant ton texte. Veuillez en informer le staff.")
 		});
 
-		// Await message -> filtre id joueur
-		// Faire les calculs de points : https://10fastfingers.com/faq
+
+		// Classement plus rapide
+
 
 	},
 };
