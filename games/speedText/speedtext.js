58b9b834ff96eb0f9a41eac41b21c3da1b7d9a43
BoBsmil3Y <antoine-dupont@gmx.fr>
2020-01-09 16:07:03 +0100

    Generate only 5 words to write. System to detect error (WIP)

--- games/speedText/speedtext.js
+++ games/speedText/speedtext.js
@@ -6,12 +6,15 @@ module.exports = {
 
 		const axios = require('axios');
 
+		let text;
 		let player = m.author;
 		let channel = m.channel;
 
-		axios.get('https://baconipsum.com/api/?type=meat-and-filler&paras=2&format=text')
+		axios.get('https://baconipsum.com/api/?type=meat-and-filler&paras=1&format=text')
 		.then(response => {
-			channel.send(response.data) // .await
+			var temp = response.data.split(/ +/);
+			text = temp[0] + " " + temp[1] + " " +  temp[2] + " " +  temp[3] + " " + temp[4];
+			channel.send(text);
 		})
 		.catch(error => {
 			console.log(error);
@@ -19,10 +22,30 @@ module.exports = {
 			.then(m => m.delete(5000));
 		});
 
-		/*channel.awaitMessages((mes => mes.author.id === player.id), { max: 1, time: 60000})
-		.then(collected => {
+
+
+		// Check the wroten text.
+		channel.awaitMessages((mes => mes.author.id === player.id), { max: 1, time: 15000})
+		.then(collected => {;
+
+			let originalText = text.toLowerCase().split(/ +/);
+			let wrotenText = collected.first().content.toLowerCase().replace('  ', ' ').split(/ +/);
+			let wrong = false;
+
+			if (wrotenText === "annuler") return channel.send("Jeu annulé !").delete(5000);
+
+			for (let i = 0; i < originalText.size; i++) {
+
+				if (!wrotenText[i] === originalText[i]) return wrong = true;
+
+			};
+
+			if(wrong) return channel.send(`Tu as fait une faute ! Tu as mal écrit le mot ${originalText[i]} ! \nTa version du mot : ${wrotenText[i]}`);
+
+			channel.send(`Bravo ${player.username} ! Ton temps est de : XX.XX s`)
 			
-		});*/
+
+		});
 
 		// Await message -> filtre id joueur
 		// Faire les calculs de points : https://10fastfingers.com/faq
